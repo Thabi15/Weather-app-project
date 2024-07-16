007cefaa937f6ccf4d33eca0d23fffd1a3999d13
Thabi15 <janemadute@gmail.com>
2024-07-16 21:34:50 +0200

    added a new function to respond to the changes when calling a different city. still to be looked at.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "2e643ee98bob615004bb88bfaet7a0aa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
 
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");

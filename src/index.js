669e73a0eb36fe6b64c2387d99706d5f42070714
Thabi15 <janemadute@gmail.com>
2024-07-16 20:51:16 +0200

    functionality part 3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
+
   cityElement.innerHTML = searchInput.value;
 }
 

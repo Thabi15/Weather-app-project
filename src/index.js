b0e1f6b32fa502a9f9ba065edcd6664eb925ce3b
Thabi15 <janemadute@gmail.com>
2024-07-16 20:10:57 +0200

    updating functionality and testing it in the console

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);

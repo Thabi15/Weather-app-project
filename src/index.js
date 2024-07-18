dc011eb8f453a80cd9515b803f5601a14addcadd
Thabi15 <janemadute@gmail.com>
2024-07-18 21:23:02 +0200

    changed id names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = doument.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
